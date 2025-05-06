5a8f00c9b80c4433f1505ca0c989813f2e305f76
Cristian <179995201+CrisLmon@users.noreply.github.com>
2025-05-05 22:34:19 -0500

    Trabajar con cadenas

---
+++ project-java-test/src/StringDemo.java
@@ -0,0 +1,13 @@
+public class StringDemo {
+    public static void main(String[] args){
+        String greeting = "Hello";
+        String name = "Alice";
+        String message = greeting + ", " + name + "!";
+
+        System.out.println(message);
+        System.out.println("The lenght of the message is: " + message.length());
+        System.out.println("The firts character is: " + message.charAt(0));
+        System.out.println("Uppercase message: " + message.toUpperCase());
+        System.out.println("First five characters: " + message.substring(0, 5));
+    }
+}
